fa0e2b4f3be1eb895aef024c9b58a82f08439ce8
innominateAtWork <36540163+innominateAtWork@users.noreply.github.com>
2024-09-10 16:51:23 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+var assembly = typeof(Program).Assembly;
+var assemblyResources = assembly.GetManifestResourceNames();
+foreach (var resourceName in assemblyResources)
+{
+    var resourceStream = assembly.GetManifestResourceStream(resourceName);
+    if (resourceStream == null) { continue; }
+    using var streamReader = new StreamReader(resourceStream);
+    var resourceText = streamReader.ReadToEnd();
+    Console.WriteLine($"{resourceName} = {resourceText}");
+}
+
